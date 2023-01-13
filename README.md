4a5b054c1894c4612e02827a0776370fdf9dc096
tolliik <itcrowd007@gmail.com>
2023-01-13 13:40:59 +0200

    changes to delete logic

---
+++ README.md
@@ -0,0 +1,5 @@
+# backblaze
+more info about service: backblaze.com
+# Description
+Simple  easy python script which logins to backblaze s3 compatatible bucket and delete files in certain folder that older than 15(for example) days.
+I do not use lifecycle rules in bucket fo oher reason.
--- del_files.py
+++ del_files.py
@@ -4,10 +4,11 @@ from botocore.config import Config
 # Project Must install Python Package:  python-dotenv
 from dotenv import load_dotenv
 import os
-import sys
 import datetime
 from datetime import datetime, timedelta
 # Return a boto3 resource object for B2 service
+
+
 def get_b2_resource(endpoint, key_id, application_key):
     b2 = boto3.resource(service_name='s3',
                         endpoint_url=endpoint,     # Backblaze endpoint
@@ -19,11 +20,11 @@ def get_b2_resource(endpoint, key_id, application_key):
     return b2
 
 
-def get_b2_client(endpoint, keyID, applicationKey):
+def get_b2_client(endpoint, key_id, application_key):
     b2_client = boto3.client(service_name='s3',
                              endpoint_url=endpoint,                # Backblaze endpoint
-                             aws_access_key_id=keyID,              # Backblaze keyID
-                             aws_secret_access_key=applicationKey)  # Backblaze applicationKey
+                             aws_access_key_id=key_id,              # Backblaze keyID
+                             aws_secret_access_key=application_key)  # Backblaze applicationKey
     return b2_client
 
 
@@ -33,17 +34,20 @@ def list_object_keys(bucket, b2, prefix, d):
         response = b2.Bucket(bucket).objects.filter(Prefix=prefix)
         return_list = []               # create empty list
         for object in response:        # iterate over response
-            if object.last_modified.replace(tzinfo = None) < d:
-            #if object.last_modified.replace(tzinfo = None) < datetime.datetime(2022,12,31,tzinfo = None):
-                #print results
+            if object.last_modified.replace(tzinfo=None) <= d:
+                # if object.last_modified.replace(tzinfo = None) < datetime.datetime(2022,12,31,tzinfo = None):
+                # print results
                 #print('File Name: %s ---- Date: %s' % (object.key, object.size))
-                return_list.append(object.key)  # for each item in response, append object.key to list
+                # for each item in response, append object.key to list
+                return_list.append(object.key)
         return return_list                  # return list of keys from response
-        print('get objects susceesful')
+        print('get objects suscessful')
     except ClientError as ce:
         print('error', ce)
 
 # Delete the specified objects from B2
+
+
 def delete_files(bucket, keys, b2):
     objects = []
     for key in keys:
@@ -56,26 +60,53 @@ def delete_files(bucket, keys, b2):
         except ClientError as ce:
             print('error', ce)
 
-def main():
-    private_bucket_name = 'backupchurche'
-    load_dotenv()  # take environment variables from .env.
+# Delete the specified object from B2 - all versions
 
+
+def delete_files_all_versions(bucket, keys, client, prefix):
+    try:
+        # SOURCE re LOGIC FOLLOWING:  https://stackoverflow.com/questions/46819590/delete-all-versions-of-an-object-in-s3-using-python
+        paginator = client.get_paginator('list_object_versions')
+        response_iterator = paginator.paginate(Bucket=bucket, Prefix=prefix)
+        for response in response_iterator:
+            versions = response.get('Versions', [])
+            versions.extend(response.get('DeleteMarkers', []))
+            for key in keys:
+                for version_id in [x['VersionId'] for x in versions if x['Key'] == key and x['VersionId'] != 'null']:
+                    print('Deleting {} version {}'.format(key, version_id))
+                    client.delete_object(
+                        Bucket=bucket, Key=key, VersionId=version_id)
+
+    except ClientError as ce:
+        print('error', ce)
+
+
+def main():
+    load_dotenv()  # take environment variables from .env.
     # get environment variables from file .env
+    private_bucket_name = os.getenv("BUCKETNAME")  # Backblaze endpoint
     endpoint = os.getenv("ENDPOINT")  # Backblaze endpoint
     key_id_private_ro = os.getenv("KEY_ID_PRIVATE_RO")  # Backblaze keyID
     application_key_private_ro = os.getenv(
         "APPLICATION_KEY_PRIVATE_RO")  # Backblaze applicationKey
+    prefix = os.getenv("PREFIX")
 
-    # Call function to return reference to B2 service using a second set of keys
-    b2_private = get_b2_resource(endpoint, key_id_private_ro, application_key_private_ro)
-    #b2_private_client = get_b2_client(endpoint, key_id_private_ro, application_key_private_ro)
-    day_to_delete = datetime.today() - timedelta(days=15)
-    prefix='FS'
-    bucket_object_keys = list_object_keys(private_bucket_name, b2_private, prefix, day_to_delete)
+    # Call function to return reference to B2 service using a set of keys
+    b2_private = get_b2_resource(
+        endpoint, key_id_private_ro, application_key_private_ro)
+    b2_private_client = get_b2_client(
+        endpoint, key_id_private_ro, application_key_private_ro)
+
+    day_to_delete = datetime.today() - timedelta(days=14)
+
+    bucket_object_keys = list_object_keys(
+        private_bucket_name, b2_private, prefix, day_to_delete)
     print('BEFORE - Bucket Contents ')
     for key in bucket_object_keys:
         print(key)
     #delete_files(private_bucket_name, bucket_object_keys, b2_private)
+    delete_files_all_versions(
+        private_bucket_name, bucket_object_keys, b2_private_client, prefix)
     print('\nAFTER - Bucket Contents ')
     my_bucket = b2_private.Bucket(private_bucket_name)
     for my_bucket_object in my_bucket.objects.filter(Prefix='FS'):
